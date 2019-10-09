29c52d3244bbbc6707361b4dc61d76dbd0f41fcb
Bailey <bsy2658@gmail.com>
2019-10-09 14:45:47 +0900

    자료구조 추가 및 수정

--- dotori.go
+++ dotori.go
@@ -7,19 +7,34 @@ import (
 )
 
 var (
-	flagAdd      = flag.Bool("add", false, "add")
-	flagID       = flag.String("id", "", "id")
-	flagTag      = flag.String("tag", "", "tag")
-	flagThumbimg = flag.String("thumbimg", "", "path of thumbnail image")
+	flagAdd = flag.Bool("add", false, "add")
+	flagRm  = flag.Bool("remove", false, "remove")
+
+	flagID         = flag.String("id", "", "id")
+	flagTag        = flag.String("tag", "", "tag")
+	flagThumbimg   = flag.String("thumbimg", "", "path of thumbnail image")
+	flagThumbmov   = flag.String("thumbmov", "", "path of thumbnail mov")
+	flagInputpath  = flag.String("inputpath", "", "input path")
+	flagOutputpath = flag.String("outputpath", "", "output path")
+	flagType       = flag.String("type", "", "type of asset")
+	flagStatus     = flag.String("status", "", "status of asset")
+	flagUpdatetime = flag.String("updatetime", "", "updated time")
 )
 
 func main() {
 	flag.Parse()
 	if *flagAdd {
 		i := Item{}
+
 		i.ID = *flagID
 		i.Tags = append(i.Tags, *flagTag)
 		i.Thumbimg = *flagThumbimg
+		i.Thumbmov = *flagThumbmov
+		i.Inputpath = *flagInputpath
+		i.Outputpath = *flagOutputpath
+		i.Type = *flagType
+		i.Status = *flagStatus
+		i.Updatetime = *flagUpdatetime
 
 		fmt.Println(i)
 	} else {
