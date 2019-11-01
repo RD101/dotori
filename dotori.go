73732cd43d4cd3b63b20deabb6024141af29770c
78963l <78963l@naver.com>
2019-11-01 19:01:52 +0900

    Revert "check_test CheckPath input"
    This reverts commit 6c8c7710427d3acc34757619655fb3a2ea9646eb, reversing
    changes made to 82d186861d1bc2d5708575bec50d407644b877e0.

--- dotori.go
+++ dotori.go
@@ -12,15 +12,16 @@ var (
 	flagAdd = flag.Bool("add", false, "add")
 	flagRm  = flag.Bool("remove", false, "remove")
 
-	flagAuthor      = flag.String("author", "", "author")
-	flagTag         = flag.String("tag", "", "tag")
-	flagDescription = flag.String("description", "", "description")
-	flagThumbimg    = flag.String("thumbimg", "", "path of thumbnail image")
-	flagThumbmov    = flag.String("thumbmov", "", "path of thumbnail mov")
-	flagInputpath   = flag.String("inputpath", "", "input path")
-	flagOutputpath  = flag.String("outputpath", "", "output path")
-	flagType        = flag.String("type", "", "type of asset")
-	flagAttributes  = flag.String("attributes", "", "detail info of file") // "key:value,key:value"
+	flagID         = flag.String("id", "", "id")
+	flagTag        = flag.String("tag", "", "tag")
+	flagThumbimg   = flag.String("thumbimg", "", "path of thumbnail image")
+	flagThumbmov   = flag.String("thumbmov", "", "path of thumbnail mov")
+	flagInputpath  = flag.String("inputpath", "", "input path")
+	flagOutputpath = flag.String("outputpath", "", "output path")
+	flagType       = flag.String("type", "", "type of asset")
+	flagStatus     = flag.String("status", "", "status of asset")
+	flagUpdatetime = flag.String("updatetime", "", "updated time")
+	flagCreatetime = flag.String("createtime", "", "created time")
 
 	flagDBIP = flag.String("dbip", "", "DB IP")
 )
@@ -30,14 +31,16 @@ func main() {
 	if *flagAdd {
 		i := Item{}
 
-		i.Author = *flagAuthor
+		i.ID = *flagID
 		i.Tags = append(i.Tags, *flagTag)
-		i.Description = *flagDescription
 		i.Thumbimg = *flagThumbimg
 		i.Thumbmov = *flagThumbmov
 		i.Inputpath = *flagInputpath
 		i.Outputpath = *flagOutputpath
 		i.Type = *flagType
+		i.Status = *flagStatus
+		i.Updatetime = *flagUpdatetime
+		i.CreateTime = *flagCreatetime
 
 		err := i.CheckError()
 		if err != nil {
