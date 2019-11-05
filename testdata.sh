16a3d63f4c5a2317f55eed6053c025cc06d20eb0
Bailey <bsy2658@gmail.com>
2019-11-05 20:11:54 +0900

    testdata.sh의 경로 정보 변경

--- testdata.sh
+++ testdata.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #DB에 아이템 추가 테스트를 위한 명령어를 모아둔 파일
-dotori -add -author bae -inputpath /LIBRARY_3D/asset/ -type nuke
-dotori -add -author woong -inputpath /LIBRARY_3D/asset/ -type maya
-dotori -add -author liah -inputpath /LIBRARY_3D/asset/ -type houdini
+dotori -add -author bae -type nuke -inputpath /library/asset -outputpath /library/asset
+dotori -add -author woong -type maya -inputpath /library/asset -outputpath /library/asset
+dotori -add -author liah -type houdini -inputpath /library/asset -outputpath /library/asset
