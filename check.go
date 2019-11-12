8b032f2b15b8a6e07e06270954ccd840c768d140
78963l <78963l@naver.com>
2019-11-12 13:56:59 +0900

    컴파일에러수정

--- check.go
+++ check.go
@@ -3,11 +3,11 @@ package main
 import "regexp"
 
 var (
-	regexRFC3339Time 		= regexp.MustCompile(`^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])T([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9][-+]\d{2}:\d{2}$`) //2019-09-09T02:46:52+09:00
+	regexRFC3339Time = regexp.MustCompile(`^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])T([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9][-+]\d{2}:\d{2}$`) //2019-09-09T02:46:52+09:00
 
-	regexPath        		= regexp.MustCompile(`^/[/_.\w]+$`)
+	regexPath = regexp.MustCompile(`^/[/_.\w]+$`)
 
-	regexItemtype_LangUS	= regexp.MustCompile(`^/[a-zA-Z]+$`)
+	regexItemtypeLangUS = regexp.MustCompile(`^[a-zA-Z]+$`)
 
-	regexDbname_LangUS		= regexp.MustCompile(`^/[a-zA-Z]+$`)
+	regexDbnameLangUS = regexp.MustCompile(`^[a-zA-Z]+$`)
 )
