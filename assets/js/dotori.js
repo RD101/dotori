88cfd445fb3ddf35066aa6b2142305b94709adca
hanwoong kim <hello@lazypic.org>
2020-01-04 22:30:55 +0900

    리뷰반영

--- assets/js/dotori.js
+++ assets/js/dotori.js
@@ -4,15 +4,13 @@ function addAttribute() {
     let childnum = document.getElementById("attributes").childElementCount
     att = att + `
     <div class="row">
-        <div class="col">
+        <div class="col pt-2">
             <div class="form-group p-0 m-0">
-                <small class="form-text text-muted">Key</small>
                 <input type="text" class="form-control" placeholder="key" value="" name="key${childnum}">
             </div>
         </div>
-        <div class="col">
+        <div class="col pt-2">
             <div class="form-group p-0 m-0">
-                <small class="form-text text-muted">Value</small>
                 <input type="text" class="form-control" placeholder="value" value="" name="value${childnum}">
             </div>			
         </div>
