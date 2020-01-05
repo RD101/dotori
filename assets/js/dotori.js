308b9c8576a3a1c1770535ef5c7779654592f452
woong <woong@lazypic.org>
2020-01-05 20:35:01 +0900

    +버튼을 누르더라도 기존값이 삭제되지 않도록함.

--- assets/js/dotori.js
+++ assets/js/dotori.js
@@ -1,20 +1,25 @@
 
 function addAttribute() {
-    let att = document.getElementById("attributes").innerHTML;
-    let childnum = document.getElementById("attributes").childElementCount
-    att = att + `
-    <div class="row">
+    let beforeAttributes = document.getElementById("attributes");
+    let childnum = document.getElementById("attributes").childElementCount;
+    let e = document.createElement('div');
+    e.className = "row";
+    html = `
         <div class="col pt-2">
             <div class="form-group p-0 m-0">
-                <input type="text" class="form-control" placeholder="key" value="" name="key${childnum}">
+                <input type="text" class="form-control" placeholder="key" value="" id="key${childnum}" name="key${childnum}" onchange="changeInput(this);">
             </div>
         </div>
         <div class="col pt-2">
             <div class="form-group p-0 m-0">
-                <input type="text" class="form-control" placeholder="value" value="" name="value${childnum}">
+                <input type="text" class="form-control" placeholder="value" value="" id="value${childnum}" name="value${childnum}" onchange="changeInput(this);">
             </div>			
         </div>
-    </div>
     `
-    document.getElementById("attributes").innerHTML = att;
+    e.innerHTML = html;
+    beforeAttributes.appendChild(e);
+}
+
+function changeInput(obj) {
+    document.getElementById(obj.id).value = obj.value;
 }
