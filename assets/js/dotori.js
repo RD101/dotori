743935aaffc27906ed583d41955d6055e955ec78
woong <woong@lazypic.org>
2020-01-08 13:55:04 +0900

    changeInput 함수 제거

--- assets/js/dotori.js
+++ assets/js/dotori.js
@@ -6,19 +6,15 @@ function addAttribute() {
     let html = `
         <div class="col pt-2">
             <div class="form-group p-0 m-0">
-                <input type="text" class="form-control" placeholder="key" value="" id="key${childnum}" name="key${childnum}" onchange="changeInput(this);">
+                <input type="text" class="form-control" placeholder="key" value="" id="key${childnum}" name="key${childnum}">
             </div>
         </div>
         <div class="col pt-2">
             <div class="form-group p-0 m-0">
-                <input type="text" class="form-control" placeholder="value" value="" id="value${childnum}" name="value${childnum}" onchange="changeInput(this);">
+                <input type="text" class="form-control" placeholder="value" value="" id="value${childnum}" name="value${childnum}">
             </div>			
         </div>
     `
     e.innerHTML = html;
     document.getElementById("attributes").appendChild(e);
-}
-
-function changeInput(obj) {
-    document.getElementById(obj.id).value = obj.value;
 }
