6f0944b56613c7b87ad59bb189c60d85b621800f
aerim.shim <aerim.shim@localhost.localdomain>
2020-01-28 12:09:30 +0900

    빈 문자열은 리스트에서 제외

--- stringfunc.go
+++ stringfunc.go
@@ -7,9 +7,17 @@ import (
 // SplitSpace 는 string 문자열을 공백을 기준으로 split하여 리스트를 반환하는 함수이다.
 func SplitBySpace(str string) []string {
 	str = strings.TrimSpace(str)
-	if len(str) == 0 {
+	if str == "" {
 		return nil
 	}
 
-	return strings.Split(str, " ")
+	// 빈 문자열은 리스트에서 제외
+	var result []string
+	for _, s := range strings.Split(str, " ") {
+		if s != "" {
+			result = append(result, s)
+		}
+	}
+
+	return result
 }
