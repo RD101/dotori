924e15140aaaa0a33870e2432ebbf98ead0e8949
woong <woong@lazypic.org>
2020-02-05 12:18:41 +0900

    build가 안되는 버그 수정

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+
+# 사용되는 리소스를 Go파일로 변경하기
+go run assets/asset_generate.go
+
 APP="dotori"
-GOOS=linux GOARCH=amd64 go build -o ./bin/linux/${APP} check.go dbapi.go dotori.go struct.go network.go http_restapi.go
-GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/${APP} check.go dbapi.go dotori.go struct.go network.go http_restapi.go
-GOOS=windows GOARCH=amd64 go build -o ./bin/windows/${APP} check.go dbapi.go dotori.go struct.go network.go http_restapi.go
+GOOS=linux GOARCH=amd64 go build -o ./bin/linux/${APP} *.go
+GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/${APP} *.go
+GOOS=windows GOARCH=amd64 go build -o ./bin/windows/${APP} *.go
 
 # Github Release에 업로드 하기위해 압축
-cd ./bin/linux/ && mkdir thumbnail && tar -zcvf ../${APP}_linux_x86-64.tgz . && cd -
-cd ./bin/darwin/ && mkdir thumbnail && tar -zcvf ../${APP}_darwin_x86-64.tgz . && cd -
-cd ./bin/windows/ && mkdir thumbnail && tar -zcvf ../${APP}_windows_x86-64.tgz . && cd -
+cd ./bin/linux/ && tar -zcvf ../${APP}_linux_x86-64.tgz . && cd -
+cd ./bin/darwin/ && tar -zcvf ../${APP}_darwin_x86-64.tgz . && cd -
+cd ./bin/windows/ && tar -zcvf ../${APP}_windows_x86-64.tgz . && cd -
 
 # 삭제
 rm -rf ./bin/linux
