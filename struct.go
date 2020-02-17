1e7a4a83bb0039586323a14d0c6b39a58ba04580
hanwoong kim <hello@lazypic.org>
2020-02-17 11:39:01 +0900

    admin setting 자료구조 추가함.

--- struct.go
+++ struct.go
@@ -5,32 +5,39 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-// Storage 는
-type Storage struct {
-	ID      string // 스토리지 ID
-	Windows string // 스토리지의 Windows 물리적 경로
-	Linux   string // 스토리지의 Linux 물리적 경로
-	MacOS   string // 스토리지의 macOS 물리적 경로
+// Adminsetting 자료구조
+type Adminsetting struct {
+	Rootpath            string `json:"rootpath" bson:"rootpath"`                       // 에셋 라이브러러리 물리경로
+	LinuxProtocolPath   string `json:"linuxprotocolpath" bson:"linuxprotocolpath"`     // 웹에서 클릭시 사용하는 Linux 경로
+	WindowsProtocolPath string `json:"windowsprotocolpath" bson:"windowsprotocolpath"` // 웹에서 클릭시 사용하는 Windows 경로
+	MacosProtocolPath   string `json:"macosprotocolpath" bson:"macosprotocolpath"`     // 웹에서 클릭시 사용하는 macOS 경로
+	Umask               string `json:"umask" bson:"umask"`                             // Umask 값
+	FolderPermission    string `json:"folderpermission" bson:"folderpermission"`       // 폴더 생성시 사용하는 권한
+	FilePermission      string `json:"filepermission" bson:"filepermission"`           // 파일 생성시 사용하는 권한
+	UID                 string `json:"uid" bson:"uid"`                                 // 유저 ID
+	GID                 string `json:"gid" bson:"gid"`                                 // 그룹 ID
+	FFmpeg              string `json:"ffmpeg" bson:"ffmpeg"`                           // FFmpeg 명령어 경로
+	OpenColorIO         string `json:"opencolorio" bson:"opencolorio"`                 // OpenColorIO 명령어 경로
+	OpenImageIO         string `json:"openimageio" bson:"openimageio"`                 // OpenImageIO 명령어 경로
 }
 
 // Item 은 라이브러리의 에셋 자료구조이다.
 type Item struct {
-	ID          bson.ObjectId                   `json:"id" bson:"_id,omitempty"`        // ID
-	Author      string                          `json:"author" bson:"author"`           // 에셋을 제작한 사람
-	Tags        []string                        `json:"tags" bson:"tags"`               // 태그리스트
-	Description string                          `json:"description" bson:"description"` // 에셋에 대한 추가 정보. 에셋의 제약, 사용전 알아야 할 특징
-	Thumbimg    string                          `json:"thumbimg" bson:"thumbimg"`       // 썸네일 이미지 주소
-	Thumbmov    string                          `json:"thumbmov" bson:"thumbmov"`       // 썸네일 영상 주소
-	Inputpath   string                          `json:"inputpath" bson:"inputpath"`     // 최초 등록되는 경로
-	Outputpath  string                          `json:"outputpath" bson:"outputpath"`   // 저장되는 경로
-	ItemType    string                          `json:"itemtype" bson:"itemtype"`       // maya, source, houdini, blender, nuke ..  같은 형태인가.
-	Status      string                          `json:"status" bson:"status"`           // 상태(에러, done, wip)
-	Log         string                          `json:"log" bson:"log"`                 // 데이터를 처리할 때 생성되는 로그
-	CreateTime  string                          `json:"createtime" bson:"createtime"`   // Item 생성 시간
-	Updatetime  string                          `json:"updatetime" bson:"updatetime"`   // UTC 타임으로 들어가도록 하기.
-	UsingRate   int64                           `json:"usingrate" bson:"usingrate"`     // 사용 빈도 수
-	Storage     `json:"storage" bson:"storage"` // Item이 저장되는 스토리지 정보
-	Attributes  map[string]string               `json:"attributes" bson:"attributes"` // 해상도, 속성, 메타데이터 등의 파일정보
+	ID          bson.ObjectId     `json:"id" bson:"_id,omitempty"`        // ID
+	Author      string            `json:"author" bson:"author"`           // 에셋을 제작한 사람
+	Tags        []string          `json:"tags" bson:"tags"`               // 태그리스트
+	Description string            `json:"description" bson:"description"` // 에셋에 대한 추가 정보. 에셋의 제약, 사용전 알아야 할 특징
+	Thumbimg    string            `json:"thumbimg" bson:"thumbimg"`       // 썸네일 이미지 주소
+	Thumbmov    string            `json:"thumbmov" bson:"thumbmov"`       // 썸네일 영상 주소
+	Inputpath   string            `json:"inputpath" bson:"inputpath"`     // 최초 등록되는 경로
+	Outputpath  string            `json:"outputpath" bson:"outputpath"`   // 저장되는 경로
+	ItemType    string            `json:"itemtype" bson:"itemtype"`       // maya, source, houdini, blender, nuke ..  같은 형태인가.
+	Status      string            `json:"status" bson:"status"`           // 상태(에러, done, wip)
+	Log         string            `json:"log" bson:"log"`                 // 데이터를 처리할 때 생성되는 로그
+	CreateTime  string            `json:"createtime" bson:"createtime"`   // Item 생성 시간
+	Updatetime  string            `json:"updatetime" bson:"updatetime"`   // UTC 타임으로 들어가도록 하기.
+	UsingRate   int64             `json:"usingrate" bson:"usingrate"`     // 사용 빈도 수
+	Attributes  map[string]string `json:"attributes" bson:"attributes"`   // 해상도, 속성, 메타데이터 등의 파일정보
 }
 
 // CheckError 는 Item 자료구조에 값이 정확히 들어갔는지 확인하는 메소드이다.
