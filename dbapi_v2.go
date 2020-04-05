1c3c19a9fa211c565da86991bc83d33014f5c86b
Bailey <bsy2658@gmail.com>
2020-04-05 17:31:33 +0900

    searchPage

--- dbapi_v2.go
+++ dbapi_v2.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // AddItem 은 데이터베이스에 Item을 넣는 함수이다.
@@ -93,4 +95,62 @@ func UpdateItem(client *mongo.Client, itemType string, item Item) error {
 		return err
 	}
 	return nil
+}
+
+// SearchPage 는 itemType, words, 해당 page를 입력받아 해당 아이템을 검색한다. 검색된 아이템과 그 개수를 반환한다.
+// http.go에서 사용중
+func SearchPage(client *mongo.Client, itemType string, words string, page, limitnum int64) (int64, int64, []Item, error) {
+	var results []Item
+	//검색어가 존재하지 않으면 빈 결과를 반환한다.
+	if words == "" {
+		return 0, 0, results, nil
+	}
+	collection := client.Database(*flagDBName).Collection(itemType)
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	wordsQueries := []bson.M{}
+	for _, word := range strings.Split(words, " ") {
+		if word == "" {
+			continue
+		}
+		querys := []bson.M{}
+		//"tag:"가 앞에 붙어있으면 태그에서 검색한다.
+		if strings.HasPrefix(word, "tag:") {
+			querys = append(querys, bson.M{"tags": strings.TrimPrefix(word, "tag:")})
+		} else if strings.Contains(word, ":") {
+			key := strings.Split(word, ":")[0]
+			value := strings.Split(word, ":")[1]
+			querys = append(querys, bson.M{"attributes." + key: primitive.Regex{Pattern: value, Options: "i"}})
+		} else {
+			querys = append(querys, bson.M{"author": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"tags": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"description": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"type": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"inputpath": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"outputpath": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"createtime": primitive.Regex{Pattern: word, Options: "i"}})
+			querys = append(querys, bson.M{"updatetime": primitive.Regex{Pattern: word, Options: "i"}})
+		}
+		wordsQueries = append(wordsQueries, bson.M{"$or": querys})
+	}
+	// 사용률이 많은 소스가 위로 출력되도록 한다.
+	q := bson.M{"$and": wordsQueries} // 최종 쿼리는 BSON type 오브젝트가 되어야 한다.
+	opts := options.Find()
+	opts.SetSort(bson.D{{"usingrate", -1}})
+	opts.SetSkip(int64((page - 1) * limitnum))
+	opts.SetLimit(int64(limitnum))
+	cursor, err := collection.Find(ctx, q, opts)
+	if err != nil {
+		return 0, 0, nil, err
+	}
+	err = cursor.All(ctx, &results)
+	if err != nil {
+		return 0, 0, nil, err
+	}
+	totalNum, err := collection.CountDocuments(ctx, q)
+	if err != nil {
+		return 0, 0, nil, err
+	}
+
+	return TotalPage(totalNum, limitnum), totalNum, results, nil
 }
