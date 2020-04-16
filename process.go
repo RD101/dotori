394e0fd76f9296a51c838ae988ae8511865e953d
Bailey <bsy2658@gmail.com>
2020-04-16 17:37:14 +0900

    함수 내부에서 status지정하는 부분을 삭제. 함수 밖에서 status를 바꿔준다

--- process.go
+++ process.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/disintegration/imaging"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -49,6 +48,10 @@ func processingItem() error {
 		fmt.Println("GetReadyItem 완료")
 	}
 	// thumbnail 폴더를 생성한다.
+	err = SetStatus(client, item, CreatingThumbDir)
+	if err != nil {
+		return err
+	}
 	err = genThumbDir(adminSetting, item)
 	if err != nil {
 		return err
@@ -56,7 +59,15 @@ func processingItem() error {
 	if *flagDebug {
 		fmt.Println("genThumbDir 완료")
 	}
+	err = SetStatus(client, item, CreatedThumbDir)
+	if err != nil {
+		return err
+	}
 	// 썸네일 이미지를 생성한다.
+	err = SetStatus(client, item, CreatingThumbImg)
+	if err != nil {
+		return err
+	}
 	err = genThumbImage(adminSetting, item)
 	if err != nil {
 		return err
@@ -64,7 +75,15 @@ func processingItem() error {
 	if *flagDebug {
 		fmt.Println("genThumbImage 완료")
 	}
+	err = SetStatus(client, item, CreatedThumbImg)
+	if err != nil {
+		return err
+	}
 	// .ogg 썸네일 동영상을 생성한다.
+	err = SetStatus(client, item, CreatingOggContainer)
+	if err != nil {
+		return err
+	}
 	err = getThumbOggContainer(adminSetting, item) // FFmpeg는 확장자에 따라 옵션이 다양하거나 호환되지 않는다. 포멧별로 분리한다.
 	if err != nil {
 		return err
@@ -73,6 +92,10 @@ func processingItem() error {
 		fmt.Println("genThumbOggContainer 완료")
 	}
 	// .mov 썸네일 동영상을 생성한다.
+	err = SetStatus(client, item, CreatingMovContainer)
+	if err != nil {
+		return err
+	}
 	err = getThumbMovContainer(adminSetting, item) // FFmpeg는 확장자에 따라 옵션이 다양하거나 호환되지 않는다. 포멧별로 분리한다.
 	if err != nil {
 		return err
@@ -81,12 +104,24 @@ func processingItem() error {
 		fmt.Println("genThumbMovContainer 완료")
 	}
 	// .mp4 썸네일 동영상을 생성한다.
+	err = SetStatus(client, item, CreatingMp4Container)
+	if err != nil {
+		return err
+	}
 	err = getThumbMp4Container(adminSetting, item) // FFmpeg는 확장자에 따라 옵션이 다양하거나 호환되지 않는다. 포멧별로 분리한다.
 	if err != nil {
 		return err
 	}
 	if *flagDebug {
 		fmt.Println("genThumbMp4Container 완료")
+	}
+	err = SetStatus(client, item, CreatedContainers)
+	if err != nil {
+		return err
+	}
+	err = SetStatus(client, item, Done)
+	if err != nil {
+		return err
 	}
 	return nil
 }
@@ -111,10 +146,6 @@ func genThumbDir(adminSetting Adminsetting, item Item) error {
 	}
 	// Status를 CreatingThumbDir로 바꾼다.
 	collection := client.Database(*flagDBName).Collection(item.ItemType)
-	_, err = collection.UpdateOne(ctx, bson.M{"_id": item.ID}, bson.M{"$set": bson.M{"status": CreatingThumbnail}})
-	if err != nil {
-		return err
-	}
 	umask, err := strconv.Atoi(adminSetting.Umask)
 	if err != nil {
 		return err
@@ -127,11 +158,6 @@ func genThumbDir(adminSetting Adminsetting, item Item) error {
 	// 생성할 경로를 가져온다.
 	path := path.Dir(item.OutputThumbnailPngPath)
 	err = os.MkdirAll(path, os.FileMode(per))
-	if err != nil {
-		return err
-	}
-	// Status를 CreatedThumbDir로 바꾼다.
-	_, err = collection.UpdateOne(ctx, bson.M{"_id": item.ID}, bson.M{"$set": bson.M{"status": CreatingThumbnail}})
 	if err != nil {
 		return err
 	}
@@ -157,11 +183,6 @@ func genThumbImage(adminSetting Adminsetting, item Item) error {
 		return err
 	}
 	collection := client.Database(*flagDBName).Collection(item.ItemType)
-	// Status를 CreatingThumbnail로 바꾼다.
-	_, err = collection.UpdateOne(ctx, bson.M{"_id": item.ID}, bson.M{"$set": bson.M{"status": CreatingThumbnail}})
-	if err != nil {
-		return err
-	}
 	// 변환할 이미지를 가져온다.
 	path := item.InputThumbnailImgPath
 	target, err := imaging.Open(path)
@@ -172,11 +193,6 @@ func genThumbImage(adminSetting Adminsetting, item Item) error {
 	result := imaging.Fill(target, 320, 180, imaging.Center, imaging.Lanczos)
 	//생성한 경로에 연산된 이미지 저장
 	err = imaging.Save(result, item.OutputThumbnailPngPath)
-	if err != nil {
-		return err
-	}
-	// Status를 CreatedThumbnail로 바꾼다.
-	_, err = collection.UpdateOne(ctx, bson.M{"_id": item.ID}, bson.M{"$set": bson.M{"status": CreatedThumbnail}})
 	if err != nil {
 		return err
 	}
