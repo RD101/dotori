e8aee9ee3dc70f86e1ab7f25f72c98d345ad764b
ChaeyunBae <qocodbssla@naver.com>
2020-05-17 15:13:02 +0900

    삭제 시 alert로 itemtype이랑 id 삭제했다고 알려줌

--- assets/js/dotori.js
+++ assets/js/dotori.js
@@ -57,10 +57,11 @@ function rmItemModal(itemtype,itemId) {
         type: "delete",
         dataType: "json",
         success: function() {
+            alert("itemtype: "+itemtype+"\nid: "+itemId+"\n아이템 삭제를 성공했습니다."); 
             location.reload();
         },
         error: function(){
-            alert("아이템 삭제를 실패했습니다.");  
+            alert("itemtype: "+itemtype+"\nid: "+itemId+"\n아이템 삭제를 실패했습니다.");  
         }
     });
 }
