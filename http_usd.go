e27f1a1462c50adfbd2c0931e227aa3d0367e621
Bailey <bsy2658@gmail.com>
2020-06-10 16:19:19 +0900

    usd 데이터체크 페이지 추가

--- http_usd.go
+++ http_usd.go
@@ -462,6 +462,18 @@ func handleUploadUSDFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleUploadUSDCheckData(w http.ResponseWriter, r *http.Request) {
+	token, err := GetTokenFromHeader(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/signin", http.StatusSeeOther)
+		return
+	}
+	type recipe struct {
+		Token
+		Adminsetting Adminsetting
+		Item         Item
+	}
+	rcp := recipe{}
+	rcp.Token = token
 	// objectID로 item을 가져온다.
 	objectID, err := GetObjectIDfromRequestHeader(r)
 	if err != nil {
@@ -487,27 +499,28 @@ func handleUploadUSDCheckData(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	//rcp에 adminsetting 추가
+	adminsetting, err := GetAdminSetting(client)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rcp.Adminsetting = adminsetting
+	//rcp에 item 추가
 	item, err := GetItem(client, objectID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// 썸네일이미지가 있는지 체크
-	if !item.ThumbImgUploaded {
-		http.Error(w, "썸네일이미지를 업로드해주세요", http.StatusBadRequest)
+	rcp.Item = item
+	if !item.ThumbImgUploaded || !item.ThumbClipUploaded || !item.DataUploaded {
+		err = TEMPLATES.ExecuteTemplate(w, "checkusd-file", rcp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		return
 	}
-	// 썸네일클립이 있는지 체크
-	if !item.ThumbClipUploaded {
-		http.Error(w, "썸네일클립을 업로드해주세요", http.StatusBadRequest)
-		return
-	}
-	// USD파일이 있는지 체크
-	if !item.DataUploaded {
-		http.Error(w, "USD 파일을 업로드해주세요", http.StatusBadRequest)
-		return
-	}
-	// addusd-success 페이지로 연결
 	http.Redirect(w, r, "/addusd-success", http.StatusSeeOther)
 }
 
