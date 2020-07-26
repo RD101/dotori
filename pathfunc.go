99c6ce3976c1981d08fa42fa5bb596b5e9e858c3
Bailey <bsy2658@gmail.com>
2020-07-27 01:03:49 +0900

    DB에 연결에서 adminsetting가져오기, outputdatapath 존재하지 않으면 만들어주기

--- pathfunc.go
+++ pathfunc.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -11,8 +12,12 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"golang.org/x/sys/unix"
 )
 
 // searchSeq 함수는 탐색할 경로를 입력받고 dpx, exr, png, mov 정보를 수집 반환한다.
@@ -187,40 +192,113 @@ func RmData(client *mongo.Client, id string) error {
 	return nil
 }
 
+// 참고한 코드: https://stackoverflow.com/a/21067803
 func copyFile(inputpath, outputpath string) error {
 	fmt.Println(inputpath)
 	fmt.Println(outputpath)
+
+	//mongoDB client 연결
+	client, err := mongo.NewClient(options.Client().ApplyURI(*flagMongoDBURI))
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return err
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return err
+	}
+
+	// adminsetting에서 폴더 생성에 필요한 값들을 가져온다.
+	adminsetting, err := GetAdminSetting(client)
+	if err != nil {
+		return err
+	}
+	// umask, 권한 셋팅
+	umask, err := strconv.Atoi(adminsetting.Umask)
+	if err != nil {
+		return err
+	}
+	unix.Umask(umask)
+	folderP := adminsetting.FolderPermission
+	folderPerm, err := strconv.ParseInt(folderP, 8, 64)
+	if err != nil {
+		return err
+	}
+	u := adminsetting.UID
+	uid, err := strconv.Atoi(u)
+	if err != nil {
+		return err
+	}
+	g := adminsetting.GID
+	gid, err := strconv.Atoi(g)
+	if err != nil {
+		return err
+	}
+
+	// input경로 검사
 	src, err := os.Stat(inputpath)
 	if err != nil {
 		return err
 	}
+	// 레귤러 파일이 아니면 에러처리 한다.
 	if !src.Mode().IsRegular() {
-		// cannot copy non-regular files (e.g., directories,
-		// symlinks, devices, etc.)
+		// cannot copy non-regular files (e.g., directories, symlinks, devices, etc.)
 		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", src.Name(), src.Mode().String())
 	}
+
+	// output경로 검사.
 	dst, err := os.Stat(outputpath)
-	if err != nil {
-		if !os.IsNotExist(err) {
+	// 경로가 존재하지 않으면 새로 만든다.
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(outputpath, os.FileMode(folderPerm))
+		if err != nil {
 			return err
 		}
-		return err
-	} else {
-		if !(dst.Mode().IsRegular()) {
-			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dst.Name(), dst.Mode().String())
-		}
-		if os.SameFile(src, dst) {
-			return nil
+		err = os.Chown(outputpath, uid, gid)
+		if err != nil {
+			return err
 		}
 	}
+	if err != nil {
+		return err
+	}
+	// // 레귤러 파일이 아니면 에러처리 한다.
+	// if !dst.Mode().IsRegular() {
+	// 	return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dst.Name(), dst.Mode().String())
+	// }
+	_, filename := path.Split(inputpath)
+	outputpath = outputpath + filename
+	fmt.Println("1")
+	// src경로와 dst경로가 같으면 옮길 필요가 없다.
+	if os.SameFile(src, dst) {
+		return nil
+	}
+	fmt.Println("2")
+	// 이건 왜 해주는거지? 논란의 여지가 있군.
 	if err = os.Link(inputpath, outputpath); err == nil {
 		return nil
 	}
-	err = copyFileContents(inputpath, outputpath)
+	fmt.Println("3")
+	err = copyFileContents(inputpath, outputpath+"maya_scene.ma")
+	if err != nil {
+		return err
+	}
+	fmt.Println("4")
 	return nil
 }
 
 func copyFileContents(inputpath, outputpath string) error {
+	fmt.Println("copyFileContents")
 	in, err := os.Open(inputpath)
 	if err != nil {
 		return err
