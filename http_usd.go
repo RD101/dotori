ae994c8d273c66d41ed3d420fec667e8577ca783
Bailey <bsy2658@gmail.com>
2020-07-31 16:35:37 +0900

    author 자동 태깅 없앰

--- http_usd.go
+++ http_usd.go
@@ -97,7 +97,6 @@ func handleUploadUSDItem(w http.ResponseWriter, r *http.Request) {
 	item.Title = r.FormValue("title")
 	item.Description = r.FormValue("description")
 	tags := SplitBySpace(r.FormValue("tag"))
-	tags = append(tags, item.Author) // author는 자동으로 태깅되도록 한다.
 	item.Tags = tags
 	item.ItemType = "usd"
 	item.KindOfUSD = r.FormValue("kindofusd")
