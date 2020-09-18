e5b5d3c51d3880aa5caa1c7282d48fae6bef4182
Bailey <bsy2658@gmail.com>
2020-09-18 18:54:23 +0900

    maya

--- assets/js/dropzone.js
+++ assets/js/dropzone.js
@@ -3656,3 +3656,21 @@ Dropzone.options.houdiniDropzone = {
     document.getElementById("btn-register-item").disabled = false; // 전송이 완료되면 등록버튼을 활성화 시킨다.
   },
 }
+
+// maya dropzone 페이지에서 업로드 버튼을 눌렀을 때 실행되는 코드부
+Dropzone.options.mayaDropzone = {
+  autoProcessQueue: false,
+  init: function () {
+    // 최초 dropzone 설정시 init을 통해 호출
+    var submitButton = document.getElementById("btn-upload-file");
+    var currentDropzone = this; //closure
+    submitButton.addEventListener("click",
+      function () {
+          currentDropzone.processQueue(); // 파일을 수동으로 전송한다.
+      }
+    );
+  },
+  success: function success() {
+    document.getElementById("btn-register-item").disabled = false; // 전송이 완료되면 등록버튼을 활성화 시킨다.
+  },
+}
