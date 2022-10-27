206d98de67993d361b22e54483014eaf06a9778a
hanwoong kim <hello@lazypic.org>
2022-10-28 01:09:19 +0900

    디테일 뷰에 카테고리 기능 추가함.

--- assets/js/dotori.js
+++ assets/js/dotori.js
@@ -98,7 +98,7 @@ function setDetailViewModal(itemid) {
                 `;
             }
             document.getElementById("modal-detailview-tags").innerHTML = tagsHtml
-            
+            document.getElementById("modal-detailview-categories").innerHTML = response["categories"].join(" > ")
             // Attributes 세팅
             if (Object.keys(response["attributes"]).length != 0) {
                 let attributesHtml = `<strong>Attributes</strong>
